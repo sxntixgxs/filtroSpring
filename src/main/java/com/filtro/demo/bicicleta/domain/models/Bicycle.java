3232bc432f6d01a6e0d410a64cd6ce882998b42a
juandavidg0mez <nihlgomez@gmail.com>
2024-09-11 00:53:43 +0200

    finsihed

---
+++ src/main/java/com/filtro/demo/bicicleta/domain/models/Bicycle.java
@@ -0,0 +1,25 @@
+package com.filtro.demo.bicicleta.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="bicicletas")
+public class Bicycle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
--- src/main/java/com/filtro/demo/model/infrastructure/ModelController.java
+++ src/main/java/com/filtro/demo/model/infrastructure/ModelController.java
@@ -1,12 +1,16 @@
 package com.filtro.demo.model.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filtro.demo.model.application.ModelServices;
@@ -27,6 +31,20 @@ public class ModelController {
     @PostMapping
     public String createModel(@RequestBody Model model){
         services.createModel(model);
-        return 
+        return "created";
+    }
+    @PutMapping
+    public String updateModel(@RequestBody Model model){
+        Optional<Model> optional = services.getModelById(model.getId());
+        if(optional.isPresent()){
+            services.updateModel(model);
+            return "updated";
+        }else{
+            return "not updated";
+        }
+    }
+    @DeleteMapping("/{id}")
+    public String deleteModel(@RequestParam Integer id){
+        return services.deleteModelById(id) ? "deleted" : "not deleted";
     }
 }
